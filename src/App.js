d5510585cea13b0d514307b85f4d6b6fe220cae1
Brandyn <stateofw@gmail.com>
2021-11-05 14:36:10 -0400

    mvp code completed

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+import Character from './components/Character'
 import './App.css';
 
+
+
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+  const [URL] = useState("https://swapi.dev/api/people")
+  const [char, setChar] = useState([])
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  useEffect(()=>{
+    axios.get(URL)
+       .then((res) => {
+         console.log('Success', res.data)
+         setChar(res.data)
+       })
+       .catch(errors => {console.log(errors)})
+   }, [])
 
-  return (
-    <div className="App">
-      <h1 className="Header">Characters</h1>
-    </div>
-  );
-}
-
-export default App;
+    return (
+      <div className="App">
+        <div>
+        <p><Character  character={char}/></p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default App;
+  
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,51 @@
 // Write your Character component here
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledDiv2 = styled.div`
+        width: 80%;
+        display: flex;
+        justify-content: space-between;
+        margin: auto;
+        padding: 8px;
+        background-color: black;
+        opacity: 0.5;
+        color: #0077b6;
+
+    p {
+        font-size: 20px;
+        color: white;
+    }
+
+`
+
+const Section = styled.section`
+    h1 {
+        font-size: 60px;
+        color: black;
+    }
+
+`
+
+const characterApp = (props) => {
+  
+    return(
+      <Section>
+          <h1>React Wars</h1>
+           {
+            props.character.map((character) => (
+               <StyledDiv2>
+               <p>{character.name}</p>
+               <p>{character.birth_year}</p>
+               </StyledDiv2>
+               
+               ))
+             
+            }
+
+      </Section>   
+    );
+  }
+
+
+export default characterApp;
